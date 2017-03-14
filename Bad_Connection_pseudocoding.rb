242b03b8ec93d3f47d1802b3e9a94b2e475f4646
squeemishly <squeemishly@gmail.com>
2017-03-14 09:08:36 -0600

    first commit

---
+++ Bad_Connection_pseudocoding.rb
@@ -0,0 +1,19 @@
+1. How will you know when the problem is solved? (Identify the big-picture goal)
+    When I have a functioning software that does what is required of it. Preferably, I will have written code that is efficient as well as effective in the process.
+
+2. How do you want to use the software? (Identify the “interface”)
+    In this case, we just want to run it in the terminal. I suppose this could be translated into a terrible text based game that was run in a new window? But that seems pointless...
+
+3. What’s the (next-)most trivial possible case? (Identify the next small-picture goal)
+    Start with having the end user being able to interact with the person on the phone. Even if the communication is worse than this particular assignment demands it to be...
+
+4. How do we achieve this goal? (Sketch an algorithm using pseudocode)
+    Start with an opening line, something like "This is a grocery store"
+    Let the user tell me what their question/statement is
+    Based on the user input allow several types of responses:
+        If they don't respond at all, shout "Hello" at them
+        If they ask a question, tell them that you're having a hard time hearing them
+        If they ask a question in all caps, tell them they have not reached a pet store
+        The first time they say goodbye, ask if they need help with anything else 
+        The second time they say goodbye, thank them for calling and leave the program 
+
