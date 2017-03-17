2a2ae14bdc61d1a3f9b0e1dc506b6cb67cc03c1d
squeemishly <squeemishly@gmail.com>
2017-03-17 11:17:00 -0600

    Found check digit

--- credit_check.rb
+++ credit_check.rb
@@ -9,9 +9,7 @@ end
 
 def multiply_by_two(cc_number)
   cc_number.each_with_index do |number, index|
-    if index == 0
-      next
-    elsif index.even?
+    if index.even?
       cc_number[index] = cc_number[index] * 2
       if cc_number[index] > 9
         cc_number[index] = cc_number[index].to_s
@@ -24,7 +22,22 @@ def multiply_by_two(cc_number)
   end
 end
 
+def find_check_digit (cc_number)
+  total = 0
+  total_ones_digit = nil
+  check_digit = nil
+  cc_number.each do |number|
+    total = total + number
+  end
+  p total
+  total_ones_digit = total.to_s
+  total_ones_digit = total_ones_digit.split('').map(&:to_i)
+  check_digit = 10 - total_ones_digit[1]
+  p check_digit
+end
+
 card_number = split_and_reverse(card_number)
 p card_number
 multiply_by_two(card_number)
 p card_number
+find_check_digit(card_number)
