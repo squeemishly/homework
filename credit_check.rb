e7f9367e743290144ec0c0b404005ef578dfd4a0
squeemishly <squeemishly@gmail.com>
2017-03-17 16:50:48 -0600

    Fuck. I just realized I was doing this all wrong.

--- credit_check.rb
+++ credit_check.rb
@@ -1,51 +1,61 @@
+require 'pry'
 card_number = "4929735477250543"
+card_number_saved = card_number
+card_numbers_added = nil
+check_digit = nil
 valid = false
 
 def split_long_number(number)
-  number = number.split('').map(&:to_i)
-  number
+  number.split('').map(&:to_i)
 end
 
-
-# def split_and_reverse (cc_number)
-#   cc_number = cc_number.split('').map(&:to_i)
-#   p cc_number
-#   cc_number = cc_number.reverse
-# end
-
 def multiply_by_two(cc_number)
-  cc_number = split_long_number(cc_number)
-  cc_number = cc_number.reverse
+  cc_number = split_long_number(cc_number).reverse
   cc_number.each_with_index do |number, index|
     if index.even?
       cc_number[index] = cc_number[index] * 2
       if cc_number[index] > 9
-        cc_number[index] = cc_number[index].to_s
-        cc_number[index] = split_long_number(cc_number[index])
+        cc_number[index] = split_long_number(cc_number[index].to_s)
         cc_number[index] = cc_number[index][0]+cc_number[index][1]
       end
-    else
-      next
     end
   end
 end
 
-def find_check_digit (cc_number)
+def card_number_total(cc_number)
   total = 0
-  total_ones_digit = nil
-  check_digit = nil
   cc_number.each do |number|
     total = total + number
   end
-  p total
-  total_ones_digit = total.to_s
-  total_ones_digit = split_long_number(total_ones_digit)
-  check_digit = 10 - total_ones_digit[1]
-  p check_digit
+  total
 end
 
-# card_number = split_and_reverse(card_number)
-# p card_number
+def find_check_digit (cc_number, card_numbers_added)
+  total = 0
+  total_ones_digit = nil
+  check_digit = nil
+  total_ones_digit = split_long_number(card_numbers_added.to_s)
+  check_digit = 10 - total_ones_digit[1]
+  check_digit
+end
+
+def evenly_divisible_by_ten(number_1, number_2)
+  card_total = number_1 + number_2
+  if card_total % 10 == 0
+    puts "This is a valid card number!"
+  else
+    puts "Nope. Try again!"
+  end
+end
+
 card_number = multiply_by_two(card_number)
 p card_number
-find_check_digit(card_number)
+
+card_numbers_added = card_number_total(card_number)
+p card_numbers_added
+
+check_digit = find_check_digit(card_number, card_numbers_added)
+p check_digit
+
+evenly_divisible_by_ten(card_numbers_added, check_digit)
+p card_number_saved
